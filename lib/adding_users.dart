294f30423632cd4546c7b2bdb7a358d745f39371
naieb <naiebyousefihamed@gmail.com>
2024-10-10 06:44:02 +0430

    started the user page

---
+++ lib/adding_users.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddingUsers extends StatefulWidget {
+  const AddingUsers({super.key});
+
+  @override
+  State<AddingUsers> createState() => _AddingUsersState();
+}
+
+class _AddingUsersState extends State<AddingUsers> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      )
+    );
+  }
+}
