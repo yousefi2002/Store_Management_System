7975b9f2884823d573cd329b766b33ee6cb20ab9
naieb <naiebyousefihamed@gmail.com>
2024-10-10 14:11:38 +0430

    adding user is designed

--- lib/adding_users.dart
+++ lib/adding_users.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddingUsers extends StatefulWidget {
@@ -9,24 +8,96 @@ class AddingUsers extends StatefulWidget {
 }
 
 class _AddingUsersState extends State<AddingUsers> {
+  TextEditingController userNameController = TextEditingController();
+  TextEditingController phoneNumberController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        TextField(),
-        TextField(),
-        ElevatedButton(
-            onPressed: () {},
-            child: Row(
-              children: [
-                Icon(
-                  Icons.save,
+      appBar: AppBar(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.teal,
+        title: Text('Add User'),
+      ),
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Positioned.fill(
+            left: 30,
+            top: 115,
+            right: 30,
+            bottom: 115,
+            child: Container(
+              width: 400,
+              height: 500,
+              decoration: BoxDecoration(
+                color: Colors.orange[300],
+                borderRadius: BorderRadius.circular(50),
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              width: 300,
+              height: 450,
+              decoration: BoxDecoration(
+                color: Colors.teal[300],
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextField(
+                      controller: userNameController,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.person_add_alt_1),
+                          label: Text('Full Name'),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25),
+                          )),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextField(
+                      controller: phoneNumberController,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.phone_android),
+                        label: Text('Phone Number'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                          )),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.save,
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text('Save'),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Text('Save'),
-              ],
-            ))
-      ],
-    ));
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
